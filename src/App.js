94159ba09777aa00a46bd56b52bf2ab52ed87645
devanisha20 <anishasharma197514@gmail.com>
2023-12-31 11:30:08 -0500

    inlinecss

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import FuncBased from './components/FuncBased'
 import ClassBasedComp from './components/ClassBasedComp'
 import JavascriptHtml from './components/JavascriptHtml'
+import Inlinecss from './components/Inlinecss'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <FuncBased/>
       <ClassBasedComp/>
       <JavascriptHtml/>
+      <Inlinecss/>
     </div>
   )
 }
---
+++ src/components/Inlinecss.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Inlinecss() {
+  return (
+    <div>
+        {/* inline style */}
+        <h1 style={{ color: "red" }}>This is the last day of 2023</h1>
+    </div>
+  )
+}
+
+export default Inlinecss
