7414ec2f2a7aed859fefee746982ca8961b7c1ee
devanisha20 <anishasharma197514@gmail.com>
2023-12-31 11:40:37 -0500

    externalCss

---
+++ src/components/ExternalCss.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./css/ExternalCss"
+
+function ExternalCss() {
+  return (
+    <div>
+        <h1>Happy New Year.<br/>
+        I hope you had a great year.
+        </h1>
+    </div>
+  )
+}
+
+export default ExternalCss
