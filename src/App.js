91f1fa3d3efa5d0747fdbd887209c5cf15b8bad1
devanisha20 <anishasharma197514@gmail.com>
2023-12-31 11:21:39 -0500

    classBased

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import FuncBased from './components/FuncBased'
+import ClassBasedComp from './components/ClassBasedComp'
 
 function App() {
   return (
     <div>
       <FuncBased/>
+      <ClassBasedComp/>
     </div>
   )
 }
---
+++ src/components/ClassBasedComp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ClassBasedComp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Class Based Component</h1>
+      </div>
+    )
+  }
+}
+
+export default ClassBasedComp
