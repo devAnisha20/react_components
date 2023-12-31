ece01604de12551dccb4f43a521b3ce2e0a63905
devanisha20 <anishasharma197514@gmail.com>
2023-12-31 11:27:05 -0500

    jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import FuncBased from './components/FuncBased'
 import ClassBasedComp from './components/ClassBasedComp'
+import JavascriptHtml from './components/JavascriptHtml'
 
 function App() {
   return (
     <div>
       <FuncBased/>
       <ClassBasedComp/>
+      <JavascriptHtml/>
     </div>
   )
 }
---
+++ src/components/JavascriptHtml.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function JavascriptHtml() {
+  return (
+    //Three types of JSX
+    //JSX Fragment
+    // <>
+    // <h1>Javascript & HTML</h1>
+    // </>
+    //div container
+    // <div>
+    //     <h1>Div container</h1>
+    // </div>
+    <React.Fragment>
+        <h1> React Fragment</h1>
+    </React.Fragment>
+  )
+}
+
+export default JavascriptHtml
